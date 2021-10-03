9aa3dc3a584eff4bab689aa2389b2f93b9453216
alicio <alicio.fcn@gmail.com>
2021-10-03 00:19:24 -0300

    Service de Pessoa

---
+++ src/main/java/com/backexemplo/service/PessoaService.java
@@ -0,0 +1,20 @@
+package com.backexemplo.service;
+
+import com.backexemplo.entity.Pessoa;
+import com.backexemplo.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository repository;
+
+    public List<Pessoa> listar() {
+        return repository.findAll();
+    }
+
+}
