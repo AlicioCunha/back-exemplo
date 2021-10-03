992c520ff39fc01f5e3674785777ca90ca3de731
alicio <alicio.fcn@gmail.com>
2021-10-03 13:25:48 -0300

    metodo de buscar todos registros de pessoa

--- src/main/java/com/backexemplo/controller/PessoaController.java
+++ src/main/java/com/backexemplo/controller/PessoaController.java
@@ -3,14 +3,12 @@ package com.backexemplo.controller;
 import com.backexemplo.entity.Pessoa;
 import com.backexemplo.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
