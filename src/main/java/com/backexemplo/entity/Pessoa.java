b3964d928d6ce9cb80058659e08a341c1d389ce1
alicio <alicio.fcn@gmail.com>
2021-10-03 00:02:35 -0300

    entidade de pessoa

---
+++ src/main/java/com/backexemplo/entity/Pessoa.java
@@ -0,0 +1,25 @@
+package com.backexemplo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String sobrenome;
+}
