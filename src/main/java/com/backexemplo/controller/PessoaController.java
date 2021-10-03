d36ccfdacb9828b2ab5b1e202edcbeebeff73821
alicio <alicio.fcn@gmail.com>
2021-10-03 00:03:07 -0300

    controller de pessoa

---
+++ src/main/java/com/backexemplo/controller/PessoaController.java
@@ -0,0 +1,9 @@
+package com.backexemplo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+}
